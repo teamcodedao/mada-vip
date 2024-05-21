0dc9bd9472119c983cb617005bbfc1f6d791be59
Nghiep <me@nghiep.dev>
2024-05-21 11:43:21 +0700

    Add super dog

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,14 @@ import clsx from 'clsx';
 import Header from './header';
 
 import banner1 from './banner-1.png';
+import superDog from './super-dog.png';
 
 export default function Home() {
   return (
     <div className='min-h-screen'>
       <Header />
       <main>
-        <div className='relative after:multi-[`absolute;inset-0;-m-24;bg-[url(./banner-1.png)];[background-size:200%];bg-no-repeat;z-[-1];blur-xl`]'>
+        <div className='relative after:multi-[`absolute;inset-0;-mt-24;bg-[url(./banner-1.png)];[background-size:200%];bg-no-repeat;z-[-1];blur-lg`]'>
           <div className={clsx('relative mx-auto max-w-screen-desktop')}>
             <Image
               src={banner1}
@@ -20,6 +21,16 @@ export default function Home() {
               className='max-h-screen w-full object-cover object-bottom'
             />
           </div>
+        </div>
+
+        <div className='relative z-10 -mt-40 flex justify-center px-5 sm:-mt-64'>
+          <Image
+            src={superDog}
+            quality={100}
+            priority
+            alt=''
+            className='w-[300px] sm:w-[500px] lg:w-[614px]'
+          />
         </div>
       </main>
       <Header />

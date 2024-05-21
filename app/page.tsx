3cebbdfd6f06c9e1148c45b34228c0cee6eade9e
Nghiep <me@nghiep.dev>
2024-05-21 13:01:52 +0700

    Add donations

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import Header from './header';
 import banner1 from './banner-1.png';
 import superDog from './super-dog.png';
 import paper from './paper.png';
+import family from './family.png';
 
 export default function Home() {
   return (
@@ -42,7 +43,8 @@ export default function Home() {
             className={clsx(
               'block rounded-full bg-primary font-luckiest-guy uppercase',
               'text-5xl lg:text-6xl xl:text-[64px]',
-              'px-10 py-3 sm:px-16 sm:py-4 lg:px-28 lg:py-6 '
+              'px-10 py-3 sm:px-16 sm:py-6 lg:px-28 lg:py-6',
+              'transition hover:bg-primary/80'
             )}
           >
             <span className='block translate-y-2'>BUY $MADA</span>
@@ -52,8 +54,67 @@ export default function Home() {
         <div className='mt-24 flex justify-center px-5 lg:mt-36'>
           <Image src={paper} alt='' />
         </div>
+
+        <div className='mt-10'>
+          <h2 className='px-5 text-center text-4xl font-bold text-white lg:text-5xl'>
+            Support Trump - Support Dog Club
+          </h2>
+          <div className='mt-10 bg-primary px-5 text-white'>
+            <div
+              className={clsx(
+                'mx-auto flex max-w-screen-desktop py-5',
+                'px-5 sm:px-10',
+                'flex-col sm:flex-row'
+              )}
+            >
+              <hgroup className='flex w-fit flex-col justify-center gap-4 font-bold'>
+                <h3 className='text-4xl lg:text-5xl'>TOKENOMICS</h3>
+                <h1 className='bg-[#0A3161] py-2 text-center text-xl lg:text-2xl'>
+                  SUPPLY: $100.000.000
+                </h1>
+              </hgroup>
+              <div
+                className={clsx(
+                  'text-balance border-white text-xl font-bold lg:text-2xl',
+                  'sm:ml-10 sm:pl-10 lg:ml-24 lg:pl-24',
+                  'max-sm:multi-[`mt-5;pt-5`]',
+                  'py-3 sm:py-5 lg:py-10',
+                  'border-t-2 sm:multi-[`border-l-2;border-t-0`]'
+                )}
+              >
+                (0%) Tax on Buys/Sells/Transfers for Marketing, Development,
+                Donations, and Liquidity.
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='mt-14 px-5'>
+          <h2 className='text-center font-luckiest-guy text-6xl font-medium text-white min-[400px]:text-7xl sm:text-8xl'>
+            DONATIONS
+          </h2>
+          <div className='flex justify-center'>
+            <hgroup
+              className={clsx(
+                'rounded-[2rem] border-[3.1px] border-white bg-primary text-center text-white',
+                'px-6 sm:px-14',
+                'py-3 sm:py-5'
+              )}
+            >
+              <h4 className='text-5xl font-bold sm:text-[50px]'>X ETH</h4>
+              <div className='text-2xl font-medium sm:text-[25px]'>
+                Donations to animal shelters
+              </div>
+            </hgroup>
+          </div>
+          <div className='mt-10 flex justify-center px-5'>
+            <Image src={family} alt='' className='max-w-[min(100%,756px)]' />
+          </div>
+        </div>
       </main>
-      <Header />
+      <div className='mt-10'>
+        <Header />
+      </div>
     </div>
   );
 }
